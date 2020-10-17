23ddb4f7f27dcbb0b8fb03c3bf448f21bfeeac03
Erik Butcher <with_a_k@mac.com>
2020-10-16 20:16:28 -0600

    Add login form

--- web/login.php
+++ web/login.php
@@ -4,5 +4,17 @@
   </head>
   <body>
     <?php include 'navbar.php'; ?>
+    <h1>TableTime</h1>
+    <div class="grid-container">
+      <div class="grid-x">
+        <div class="cell">
+          <form name="login" action="index.php" method="POST">
+            Username: <input type="text" name="user-name">
+            Password: <input type="password" name="password">
+            <button type="button" class="button">Log In</button>
+          </form>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
