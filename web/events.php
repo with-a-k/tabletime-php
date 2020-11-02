1b95c2c4dda152e151c57fe920d03ad68b04dc35
Erik Butcher <with_a_k@mac.com>
2020-11-02 14:51:19 -0700

    Try linking to event page

--- web/events.php
+++ web/events.php
@@ -2,6 +2,7 @@
 session_start();
 include 'queries.php';
 $otes = getOneTimeEvents();
+$recs = getRecurringEvents();
 ?>
 <html>
   <head>
@@ -12,7 +13,12 @@ $otes = getOneTimeEvents();
     <?php include 'navbar.php'; ?>
     <h1>TableTime</h1>
     <div class="grid-container">
-      <?php print_r($otes); ?>
+      <ul>
+      <?php foreach ($otes as $event): ?>
+        <li><a href="events/<?=$event['id']?>.php">Link to event page</a></li>
+      <?php endforeach; ?>
+      </ul>
+      <?php print_r($recs); ?>
     </div>
   </body>
 </html>
