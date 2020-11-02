8f05837f6380ce810e6a6d575189c788d6b5d52f
Erik Butcher <with_a_k@mac.com>
2020-11-02 13:28:49 -0700

    Try loading one-time events

--- web/events.php
+++ web/events.php
@@ -1,4 +1,8 @@
-<?php session_start(); ?>
+<?php
+session_start();
+include 'queries.php';
+$otes = getOneTimeEvents();
+?>
 <html>
   <head>
     <title>TableTime</title>
@@ -6,5 +10,9 @@
   </head>
   <body>
     <?php include 'navbar.php'; ?>
+    <h1>TableTime</h1>
+    <div class="grid-container">
+      <?php print_r($otes); ?>
+    </div>
   </body>
 </html>
