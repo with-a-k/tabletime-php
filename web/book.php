cfb392b8346b06b53a37f86d77147af08153805d
Erik Butcher <with_a_k@mac.com>
2020-11-07 23:03:14 -0700

    Fix add booking to recurring query helper

--- web/book.php
+++ web/book.php
@@ -7,7 +7,7 @@ if ($eventType == 'recurring') {
   $rid = htmlspecialchars($_POST['event_id']);
   $dayOfWeek = htmlspecialchars($_POST['day_of_week']);
   $hourOfDay = htmlspecialchars($_POST['hour_of_day']);
-  addBookingToRecurringEvent();
+  addBookingToRecurringEvent($userId, $rid, $dayOfWeek, $hourOfDay, $duration);
 } elseif ($eventType == 'one-time') {
   $oid = htmlspecialchars($_POST['event_id']);
   $dateTime = htmlspecialchars($_POST['date_time']);
